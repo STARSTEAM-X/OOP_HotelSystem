c4c5ab5893052eac8a0f794c5241ca986dcc72ad
STARSTEAM_X <STARSTEAM097097@gmail.com>
2025-02-24 04:44:02 +0700

    Update Main.py

---
+++ Backend/Main.py
@@ -0,0 +1,32 @@
+class HotelSystem:
+    pass
+
+class Room:
+    pass
+
+class User:
+    pass
+
+class Admin:
+    pass
+
+class Customer:
+    pass
+
+class Booking:
+    pass
+
+class Payment:
+    pass
+
+class Invoice:
+    pass
+
+class Discount:
+    pass
+
+class Review:
+    pass
+
+class Feedback:
+    pass
